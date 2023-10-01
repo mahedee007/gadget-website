de515d101831909171f6ece047d54161a12aff3e
Mahedee Hasan <mahedee007@gmail.com>
2023-10-02 03:15:48 +0600

    show-more function added

--- js/app.js
+++ js/app.js
@@ -9,8 +9,23 @@ const loadPhones = async(search)=>{
 const displayPhones = phones =>{
     const phoneContainer = document.getElementById ('phone-container');
     phoneContainer.textContent = '';
-
-    phones = phones.slice(0,6);
+    const showMore = document.getElementById ('show-more');
+    if(phones.length > 6){
+        phones = phones.slice(0,6);
+        showMore.classList.remove('d-none');
+    }else{
+        showMore.classList.add('d-none')
+    }
+    
+        
+        
+    
+    const warning = document.getElementById('warning');
+    if(phones.length === 0){
+        warning.classList.remove('d-none')
+    }else{
+        warning.classList.add('d-none')
+    }
 
     phones.forEach(phone =>{
         const phoneDiv = document.createElement ('div');
@@ -19,18 +34,46 @@ const displayPhones = phones =>{
         <img src=${phone.image} class="card-img-top img-fluid" alt="...">
         <div class="card-body text-center">
           <h5 class="card-title">${phone.phone_name}</h5>
-          <p class="card-text">${phone.slug}</p>
-          <button class="btn btn-primary">Show Details</button>
+          
+          <button onclick= showDetails('${phone.slug}') class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Show Details</button>
         </div>`;
         phoneContainer.appendChild(phoneDiv);
     })
-
+    toggleSpinner(false);
 }
 
 document.getElementById('search-btn').addEventListener('click', function(){
+    toggleSpinner(true);
     const searchText = document.getElementById('search-text');
     const search = searchText.value;
+    
     loadPhones(search);
+    searchText.value = '';
 })
+// document.getElementById('show-more').addEventListener('click', function(){
 
-// loadPhones();
+// })
+
+const toggleSpinner = isLoading =>{
+    const spinner = document.getElementById('spinner');
+    if(isLoading){
+        spinner.classList.remove('d-none')
+    }else{
+        spinner.classList.add('d-none')
+    }
+
+}
+
+const showDetails = async id =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayData(data.data);
+}
+const displayData = phone =>{
+    const title = document.getElementById('modal-title');
+    title.innerText = `${phone.name}`;
+    const body = document.getElementById('body');
+    body.innerHTML= `<p>${phone.others.WLAN}</p>`
+}
+// loadPhones('apple');
